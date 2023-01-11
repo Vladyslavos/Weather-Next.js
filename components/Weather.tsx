0a35e1cafa1bb45e9d0ef79a3ce6c61b4222b670
Vladyslavos <you@example.com>
2023-01-11 16:10:37 +0200

    update for p

--- components/Weather.tsx
+++ components/Weather.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function Weather() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Weather app</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis quo
+        et cupiditate quibusdam harum consequatur assumenda adipisci! Laborum,
+        officiis ipsa.
+      </p>
+    </div>
+  );
 }
