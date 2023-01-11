65b932f359e688ee9cb63b6f94e2f8ef1a9d93b4
Vladyslavos <you@example.com>
2023-01-12 00:58:28 +0200

    Data from API was recieved

--- components/Weather.tsx
+++ components/Weather.tsx
@@ -2,16 +2,26 @@ import React from "react";
 import Head from "next/head";
 import { BsSearch } from "react-icons/bs";
 import axios from "axios";
+import { IData } from "./types/type";
 
-export default function Weather(props: any) {
-  const [location, setLocation] = React.useState<string>("");
+export default function Weather() {
+  const [location, setLocation] = React.useState<string>("rome");
   const [loading, setLoading] = React.useState<boolean>(false);
+  const [data, setData] = React.useState<any>([]);
   const url: string = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
   const fetchData = (e: any) => {
-    e.preventDefault();
-    setLoading(true);
-    const response = axios.get(url);
+    try {
+      e.preventDefault();
+      setLoading(true);
+      axios.get(url).then((res) => {
+        setData(res.data);
+        console.log(data);
+      });
+    } catch (e) {
+      console.log("error");
+    }
   };
+
   return (
     <div>
       <Head>Weather on Next</Head>
@@ -26,9 +36,13 @@ export default function Weather(props: any) {
             }}
           />
         </form>
-        <button onClick={fetchData}>FetchData</button>
+        <button onClick={fetchData}>
+          <BsSearch />
+        </button>
       </div>
-      <div className="weather-content"></div>
+      <div className="weather-content">
+        <h2>{data?.weather?.[0].description}</h2>
+      </div>
     </div>
   );
 }
